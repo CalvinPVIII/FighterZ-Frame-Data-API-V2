7ed833723ae7ed361af1452780426a5b014d4de2
unknown <calvinp003@gmail.com>
2020-02-28 10:01:25 -0800

    fill out table migration and update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 8:00 - 8:30: Setup initial api
 8:30 - 9:15 Research best resource for frame data
+9:15 - 10 Research more frame data and json objects for rails
--- db/migrate/20200228164618_add_characters_table.rb
+++ db/migrate/20200228164618_add_characters_table.rb
@@ -2,6 +2,25 @@ class AddCharactersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :image1
+      t.string: :tierPlacement,
+      t.string :image1,
+      t.string :image2,
+      t.string :icon,
+      t.json :5L,
+      t.json :5LL,
+      t.json :5LLL,
+      t.json :5M,
+      t.json :2M,
+      t.json :5H,
+      t.json :2H,
+      t.json :5S,
+      t.json :236L,
+      t.json :236M,
+      t.json :236H,
+      t.json :214L,
+      t.json :214M,
+      t.json :214H,
+      t.json :214S,
+      t.json :236S
   end
 end
