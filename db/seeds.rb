ec054230f3a04d9154936dfb4d1711c154971d8c
unknown <calvinp003@gmail.com>
2020-03-01 05:00:31 -0800

    add android 21 and vegeta

--- db/seeds.rb
+++ db/seeds.rb
@@ -1541,6 +1541,406 @@ yamcha = Character.create!(
     }
 
     )
+ssjVegeta = Character.create!(
+  name: 'Super Saiyan Vegeta',
+  tier: 'A',
+  image1: '',
+  image2: '',
+  icon: '',
+  l: {
+    '5L': {
+      startup: 6,
+      advantage: -3,
+      hit: 'mid'
+    },
+    '5LL': {
+      startup: 10,
+      advantage: -2,
+      hit: 'mid'
+    },
+    '5LLL': {
+      startup: 12,
+      advantage: -5,
+      hit: 'mid'
+    },
+    '2L': {
+      startup: 7,
+      advantage: -4,
+      hit: 'low'
+    },
+    'jL': {
+      startup: 6,
+      advantage: 0,
+      hit: 'high'
+    },
+    'j2L': {
+      startup: 17,
+      advantage: 0,
+      hit: 'mid'
+    }
+  },
+  m: {
+    '5M': {
+      startup: 9,
+      advantage: -6,
+      hit: 'mid'
+    },
+    '2M': {
+      startup: 10,
+      advantage: -6,
+      hit: 'low'
+    },
+    'jM': {
+      startup: 10,
+      advantage: 0,
+      hit: 'high'
+    }
+  },
+  h: {
+    '5H': {
+      startup: 14,
+      advantage: -8,
+      hit: 'mid'
+    },
+    '2H': {
+      startup: 13,
+      advantage: -17,
+      hit: 'mid'
+    },
+    'jH': {
+      startup: 13,
+      advantage: 0,
+      hit: 'high'
+    },
+    'j2H': {
+      startup: 12,
+      advantage: 0,
+      hit: 'mid'
+    }
+  },
+  s: {
+    '5S': {
+      startup: 13,
+      advantage: -4,
+      hit: 'mid'
+    },
+    'jS': {
+      startup: 12,
+      advantage: 0,
+      hit: 'mid'
+    }
+  },
+  specials: {
+    '236L': {
+      startup: 11,
+      advantage: -5,
+      hit: 'mid'
+    },
+    '236M': {
+      startup: 11,
+      advantage: -9,
+      hit: 'mid'
+    },
+    '236H': {
+      startup: 11,
+      advantage: -13,
+      hit: 'mid'
+    },
+    'j236L': {
+      startup: 10,
+      advantage: 0,
+      hit: 'mid'
+    },
+    'j236M': {
+      startup: 11,
+      advantage: 0,
+      hit: 'mid'
+    },
+    'j236H': {
+      startup: 10,
+      advantage: 0,
+      hit: 'mid'
+    },
+    '214L': {
+      startup: 9,
+      advantage: -28,
+      hit: "mid"
+    },
+    '214M': {
+      startup: 12,
+      advantage: -30,
+      hit: 'mid',
+    },
+    '214H': {
+      startup: 9,
+      advantage: -41,
+      hit: 'mid'
+    },
+    'j214L': {
+      startup: 9,
+      advantage: 0,
+      hit: "mid"
+    },
+    'j214M': {
+      startup: 12,
+      advantage: 0,
+      hit: 'mid',
+    },
+    'j214H': {
+      startup: 9,
+      advantage: 0,
+      hit: 'mid'
+    },
+    '236S': {
+      startup: 18,
+      advantage: -4,
+      hit: 'mid'
+    },
+    'j236S': {
+      startup: 16,
+      advantage: 1,
+      hit: 'mid'
+    }
+  },
+  assist: {
+    a: {
+      startup: 38,
+      blockstun: 46
+    },
+    b: {
+      startup: 26,
+      blockstun: 31
+    },
+    c: {
+      startup: 46,
+      blockstun: 18
+    }
+  },
+  super: {
+    '236LM': {
+      startup: 9,
+      advantage: -27
+    },
+    'j236LM': {
+      startup: 9,
+      advantage: -26
+    },
+    '214LM': {
+      startup: 14,
+      advantage: -34
+    },
+    'j214LM': {
+      startup: 14,
+      advantage: -34
+    }
+  },
+  bnbs: {
+    midscreen: '',
+    corner: '',
+    blockstring: '',
+  }
+
+  )
+android21 = Character.create!(
+  name: 'Android 21',
+  tier: 'A',
+  image1: '',
+  image2: '',
+  icon: '',
+  l: {
+    '5L': {
+      startup: 6,
+      advantage: -3,
+      hit: 'mid'
+    },
+    '5LL': {
+      startup: 9,
+      advantage: -4,
+      hit: 'mid'
+    },
+    '5LLL': {
+      startup: 13,
+      advantage: -5,
+      hit: 'mid'
+    },
+    '2L': {
+      startup: 7,
+      advantage: -2,
+      hit: 'low'
+    },
+    'jL': {
+      startup: 6,
+      advantage: 0,
+      hit: 'high'
+    },
+  },
+  m: {
+    '5M': {
+      startup: 10,
+      advantage: -10,
+      hit: 'mid'
+    },
+    '2M': {
+      startup: 11,
+      advantage: -10,
+      hit: 'low'
+    },
+    '3M': {
+      startup: 17,
+      advantage: -10,
+      hit: 'low'
+    },
+    'jM': {
+      startup: 10,
+      advantage: 0,
+      hit: 'high'
+    }
+  },
+  h: {
+    '5H': {
+      startup: 15,
+      advantage: -8,
+      hit: 'mid'
+    },
+    '2H': {
+      startup: 16,
+      advantage: -19,
+      hit: 'mid'
+    },
+    'jH': {
+      startup: 14,
+      advantage: 0,
+      hit: 'high'
+    },
+    'j2H': {
+      startup: 13,
+      advantage: 0,
+      hit: 'mid'
+    }
+  },
+  s: {
+    '5S': {
+      startup: 13,
+      advantage: -4,
+      hit: 'mid'
+    },
+    '2S': {
+      startup: 21,
+      advantage: 6,
+      hit: 'mid'
+    },
+    'jS': {
+      startup: 18,
+      advantage: 0,
+      hit: 'mid'
+    }
+  },
+  specials: {
+    '236L': {
+      startup: 18,
+      advantage: -19,
+      hit: 'mid'
+    },
+    '236M': {
+      startup: 18,
+      advantage: -4,
+      hit: 'mid'
+    },
+    '236H': {
+      startup: 13,
+      advantage: [4,9,14,19],
+      hit: 'mid'
+    },
+    '214L': {
+      startup: 12,
+      advantage: -5,
+      hit: "mid"
+    },
+    '214M': {
+      startup: 14,
+      advantage: -5,
+      hit: 'mid',
+    },
+    '214H': {
+      startup: 14,
+      advantage: -5,
+      hit: 'mid'
+    },
+    'j214L': {
+      startup: 13,
+      advantage: -5,
+      hit: "mid"
+    },
+    'j214M': {
+      startup: 15,
+      advantage: -5,
+      hit: 'mid',
+    },
+    'j214H': {
+      startup: 13,
+      advantage: -5,
+      hit: 'mid'
+    },
+    '236S': {
+      startup: 27,
+      advantage: 0,
+      hit: 'throw'
+    },
+    'j236S': {
+      startup: 13,
+      advantage: 0,
+      hit: 'throw'
+    },
+    '214S': {
+      startup: 13,
+      advantage: 0,
+      hit: 'throw'
+    },
+    'j214S': {
+      startup: 13,
+      advantage: 0,
+      hit: 'throw'
+    },
+  },
+  assist: {
+    a: {
+      startup: 31,
+      blockstun: 25
+    },
+    b: {
+      startup: 26,
+      blockstun: 29
+    },
+    c: {
+      startup: 46,
+      blockstun: 50
+    }
+  },
+  super: {
+    '236LM': {
+      startup: 9,
+      advantage: 0
+    },
+    'j236LM': {
+      startup: 9,
+      advantage: 0
+    },
+    '214LM': {
+      startup: 11,
+      advantage: 0
+    },
+    'j214LM': {
+      startup: 11,
+      advantage: 0
+    }
+  },
+  bnbs: {
+    midscreen: '',
+    corner: '',
+    blockstring: '',
+  }
+
+  )
 puts 'success'
 # create_table :characters do |t|
 #   t.string :name
