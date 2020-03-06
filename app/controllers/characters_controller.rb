8e446cc3df2d21b44f51d379149f6c58342cfa7f
unknown <calvinp003@gmail.com>
2020-03-06 12:34:08 -0800

    add ability to search by tiers

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,6 +3,8 @@ class CharactersController < ApplicationController
   def index
     if params[:name] != nil
       @characters = Character.where("character -> 'bio' ->> 'name' like ?", params[:name].titleize)
+    elsif params[:tier] != nil
+        @characters = Character.where("character -> 'bio' ->> 'tier' like ?", params[:tier].titleize)
     else
       @characters = Character.all
     end
